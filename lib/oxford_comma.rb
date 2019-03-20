2954d8267edad37b69fabf1d6714d3b2e3db0df8
Dhara Patel <1991dharapatel@gmail.com>
2019-03-20 02:32:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     for i in 0...array.length-1
     result << "#{array[i]}, "
   end
-  return result << " and #{array.last}"
+  return result << "and #{array.last}"
     
   end
   array.join( " and " )
